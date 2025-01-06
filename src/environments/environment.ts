4d07324b9ccadadf817be3f4f05aa5fec9e1b14a
Danial Ali <44068314+TheOGApplePie@users.noreply.github.com>
2025-01-06 08:29:00 -0500

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDTT6BiLYkRFUPSqSE7Rnutnq-UcD3pd3Q',
+    authDomain: 'hours-of-service-8b7ed.firebaseapp.com',
+    projectId: 'hours-of-service-8b7ed',
+    storageBucket: 'hours-of-service-8b7ed.firebasestorage.app',
+    messagingSenderId: '338467876388',
+    appId: '1:338467876388:web:98ea111712f6fa6ee0f2b1',
+    measurementId: 'G-TX0RXBDE61',
+  },
+};
