00600883849ea9c6fb7b2cae2c6f78df64d4af13
Danial Ali <44068314+TheOGApplePie@users.noreply.github.com>
2025-01-06 08:32:23 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDTT6BiLYkRFUPSqSE7Rnutnq-UcD3pd3Q',
-    authDomain: 'hours-of-service-8b7ed.firebaseapp.com',
-    projectId: 'hours-of-service-8b7ed',
-    storageBucket: 'hours-of-service-8b7ed.firebasestorage.app',
-    messagingSenderId: '338467876388',
-    appId: '1:338467876388:web:98ea111712f6fa6ee0f2b1',
-    measurementId: 'G-TX0RXBDE61',
+    apiKey: 'your-key-here',
+    authDomain: 'your-ley-here',
+    projectId: 'your-key-here',
+    storageBucket: 'your-key-here',
+    messagingSenderId: 'your-key-here',
+    appId: 'your-key-here',
+    measurementId: 'your-key-here',
   },
 };
