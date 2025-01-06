f9e5ea3aa00e58e2aa837233ebd1743d52b0d833
Danial Ali <danialali@MacBook-Pro-de-Danial.local>
2025-01-05 19:42:08 -0500

    basic login complete

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private afAuth: AngularFireAuth) {}
+
+  // Sign in with Email
+  signInWithEmail(email: string, password: string) {
+    return this.afAuth.signInWithEmailAndPassword(email, password);
+  }
+
+  // Sign out
+  signOut() {
+    return this.afAuth.signOut();
+  }
+
+  // Get current user
+  getCurrentUser() {
+    return this.afAuth.authState;
+  }
+}
