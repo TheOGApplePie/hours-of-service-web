68db8368bf969c543804755c24638549cbb90e80
Danial Ali <44068314+TheOGApplePie@users.noreply.github.com>
2025-01-06 08:33:22 -0500

    The og apple pie patch 1 (#3)
    * Add files via upload
    
    * Update environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'your-key-here',
+    authDomain: 'your-ley-here',
+    projectId: 'your-key-here',
+    storageBucket: 'your-key-here',
+    messagingSenderId: 'your-key-here',
+    appId: 'your-key-here',
+    measurementId: 'your-key-here',
+  },
+};
