696fb0f921b21838c1d6eb4bf19698ba18113ad7
danial ali <thedanial.ali@gmail.com>
2025-03-28 15:07:08 -0400

    addressed the graph view

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  api_url: 'https://vtkccwmbdzhiplakswlz.supabase.co',
+  api_url: "https://vtkccwmbdzhiplakswlz.supabase.co",
+  // api_url: "http://localhost:3000",
 };
