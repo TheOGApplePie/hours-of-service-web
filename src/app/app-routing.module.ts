2910685e47bc13bf46bc60bd6d8a20c60e37ab71
danial ali <thedanial.ali@gmail.com>
2025-03-28 15:20:11 -0400

    test commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,27 +3,27 @@ import { RouterModule, type Routes } from "@angular/router";
 import { LoginGuard } from "./guards/login.guard";
 
 const routes: Routes = [
-  {
-    path: "login",
-    loadChildren: () =>
-      import("./modules/auth/auth.module").then((m) => m.AuthModule),
-  },
-  {
-    path: "",
-    loadChildren: () =>
-      import("./modules/logged-in/logged-in.module").then(
-        (m) => m.LoggedInModule,
-      ),
-    // canActivate: [LoginGuard],
-  },
-  {
-    path: "**",
-    redirectTo: "",
-  },
+	{
+		path: "login",
+		loadChildren: () =>
+			import("./modules/auth/auth.module").then((m) => m.AuthModule),
+	},
+	{
+		path: "",
+		loadChildren: () =>
+			import("./modules/logged-in/logged-in.module").then(
+				(m) => m.LoggedInModule,
+			),
+		// canActivate: [LoginGuard],
+	},
+	{
+		path: "**",
+		redirectTo: "",
+	},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
 export class AppRoutingModule {}
